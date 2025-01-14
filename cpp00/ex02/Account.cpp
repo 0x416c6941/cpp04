cf6dae85d2e07f9d1928617bd7834c07581eb797
Asset Sagymbaev <69522068+0x416c6941@users.noreply.github.com>
2025-01-14 20:19:43 +0100

    cpp00/ex02: Placebo

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -4,10 +4,11 @@
 #include <iostream>
 #include <iomanip>
 
-int Account::_nbAccounts = 0;
-int Account::_totalAmount = 0;
-int Account::_totalNbDeposits = 0;
-int Account::_totalNbWithdrawals = 0;
+// Static members are just a syntactic sugar for global variables.
+int Account::_nbAccounts;
+int Account::_totalAmount;
+int Account::_totalNbDeposits;
+int Account::_totalNbWithdrawals;
 
 Account::Account(int initial_deposit)
     :   _accountIndex(_nbAccounts++), _amount(initial_deposit),
