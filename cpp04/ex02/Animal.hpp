43c4123a942e126048543fc5ee6c30d31ef4ed64
Asset Sagymbaev <69522068+0x416c6941@users.noreply.github.com>
2025-01-18 01:00:14 +0100

    cpp04/ex02

---
+++ cpp04/ex02/Animal.hpp
@@ -0,0 +1,24 @@
+#ifndef ANIMAL_HPP
+#define ANIMAL_HPP
+
+#include <string>
+#include <Brain.hpp>
+
+class Animal {
+    protected:
+        std::string m_type;
+
+        Brain * m_brain;
+
+    public:
+        Animal(const std::string & type);
+        Animal(const Animal & src);
+        Animal & operator = (const Animal & src);
+        virtual ~Animal();
+
+        const std::string & getType() const;
+        virtual void makeSound() const = 0;
+        Brain & get_brain();
+};
+
+#endif  // ANIMAL_HPP
